452f59161cfa976a956dff06972530d67b599a72
xBrayann <brayangomez77208@gmail.com>
2025-09-26 13:08:30 -0600

    Subo Back y Front

--- BackEnd/obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net9.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67624d90b8a48f39ec849c8816402b5cb7a2cd7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
